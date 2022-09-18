ef7a9049c9211aa278a2c69024afdeda2c6cf775
Klyrasel <KaiLyrasel@outlook.com>
2022-09-18 03:15:53 -0400

    first commit

--- public/index.js
+++ public/index.js
@@ -1,9 +1,32 @@
+const { Chart } = require("chart.js");
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&2df3d8a21c144add82895c75d649df67')
+
+    const { GME, MSFT, DIS, BNTX } = mockData;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    console.log(Chart);
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor:  'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)'
+            }]
+        }
+    });
+    console.log(stocks[0].values)                                            
 }
 
 main()
